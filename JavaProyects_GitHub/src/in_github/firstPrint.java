9934bc8fd9b0465a2ec33515eefa840af373de5e
Luzz00 <83696436+Luzz00@users.noreply.github.com>
2021-05-05 12:49:59 +0200

    Cambiar nombre de paquete y primer fichero .java

---
+++ JavaProyects_GitHub/src/in_github/firstPrint.java
@@ -0,0 +1,27 @@
+/*
+Para añadir un proyecto de java a un repositorio de GitHub
+en team>git> colocar dirección del repositorio 
+
+en poryecto :  git>add
+                git>commit
+                git>push -> para que los files se suban al repositorio de HitHub
+Cuando realizamos cambios sobre alguno fichero del poyecto, para subir cambios:
+    git>add, git>commit (para guardar cambios), git>remote>push (subir cambios)
+ */
+package in_github;
+
+/**
+ *
+ * @author lcalv
+ */
+public class firstPrint {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+    
+}
